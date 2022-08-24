ed7f6b2049139a4f8b417a9dddc1f4102338380a
Dan Hamilton <hamiltondan93@gmail.com>
2022-08-24 21:47:13 +0000

    Add: start to get the width and height of maze

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,21 +8,21 @@ function navSmall() {
   }
 
   // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("modalMazeDetails");
 
 // Get the button that opens the modal
-var btn = document.getElementById("mazePropertiesBtn");
+var openModalBtn = document.getElementById("mazePropertiesBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var closeModal = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
+openModalBtn.onclick = function() {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+closeModal.onclick = function() {
   modal.style.display = "none";
 }
 
@@ -30,5 +30,26 @@ span.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+
+// Get the button that generates the maze
+let mazeGenerateBtn = document.getElementById("generateMaze");
+console.log(mazeGenerateBtn.value);
+// When the user clicks the button, make maze
+mazeGenerateBtn.addEventListener('click',make_maze);
+
+//Get element by id
+function getElId(elId) { return document.getElementById(elId) }
+
+// Make maze
+function make_maze() {
+  var w = parseInt(getElId('rows').value || 10);
+  var h = parseInt(getElId('cols').value || 10);
+  if(isNaN(w)===true || isNaN(h)===true){
+    getElId("errorMazeGenerate").style.display="block"
+    getElId("errorMazeGenerate").innerHTML = "Width and Heigth must be a number!";
+  }else{
+    getElId("errorMazeGenerate").style.display="none"
   }
 }
